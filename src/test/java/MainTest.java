14d3d8a924ff06750aceb8628bcd28887fe15815
Gregor Dirks <gregor_renke_adrian@mailbox.tu-dresden.de>
2022-05-18 15:05:29 +0200

    Aufgabe: Alarm

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -4,92 +4,41 @@ import org.junit.jupiter.api.Test;
 class MainTest {
 
     @Test
-    void shouldBeTrueIfResultOfTwoNumbersAddsCorrectlyCaseBothPositive(){
+    void zuVielePersonen() {
+        // Given
+        int test_a = 535;
+        String test_personenAnzahl = "Zu viele Personen";
+
+        // When
+        String actual = Main.personenAnzahl(test_a);
+
+        // Then
+        assertEquals(test_personenAnzahl, actual);
+    }
+
+    @Test
+    void maximalePersonenzahlNichtUeberschritten() {
         // Given
         int test_a = 4;
-        int test_b = 3;
-        int test_sum = 7;
+        String test_personenAnzahl = "Maximale Personenzahl nicht überschritten";
 
         // When
-        int actual = Main.dritte(test_a, test_b);
+        String actual = Main.personenAnzahl(test_a);
 
         // Then
-        assertEquals(test_sum, actual);
+        assertEquals(test_personenAnzahl, actual);
     }
 
-    void shouldBeTrueIfResultOfTwoNumbersAddsCorrectlyCaseSecondNegative(){
+    @Test
+    void gleichVielePersonen() {
         // Given
-        int test_a = 4;
-        int test_b = -3;
-        int test_sum = 1;
+        int test_a = 30;
+        String test_personenAnzahl = "";
 
         // When
-        int actual = Main.dritte(test_a, test_b);
+        String actual = Main.personenAnzahl(test_a);
 
         // Then
-        assertEquals(test_sum, actual);
+        assertEquals(test_personenAnzahl, actual);
     }
-
-    void shouldBeTrueIfResultOfTwoNumbersAddsCorrectlyCaseFirstNegative(){
-        // Given
-        int test_a = -4;
-        int test_b = 3;
-        int test_sum = -1;
-
-        // When
-        int actual = Main.dritte(test_a, test_b);
-
-        // Then
-        assertEquals(test_sum, actual);
-    }
-
-    void shouldBeTrueIfResultOfTwoNumbersAddsCorrectlyCaseBothNegativ(){
-        // Given
-        int test_a = -4;
-        int test_b = -3;
-        int test_sum = -7;
-
-        // When
-        int actual = Main.dritte(test_a, test_b);
-
-        // Then
-        assertEquals(test_sum, actual);
-    }
-
-    void shouldBeTrueIfResultOfTwoNumbersAddsCorrectlyCaseSmaller(){
-        // Given
-        int test_a = 4;
-        boolean test_größer = false;
-
-        // When
-        boolean actual = Main.vierte(test_a);
-
-        // Then
-        assertEquals(test_größer, actual);
-    }
-
-    void shouldBeTrueIfResultOfTwoNumbersAddsCorrectlyCaseEqual(){
-        // Given
-        int test_a = 100;
-        boolean test_größer = false;
-
-        // When
-        boolean actual = Main.vierte(test_a);
-
-        // Then
-        assertEquals(test_größer, actual);
-    }
-
-    void shouldBeTrueIfResultOfTwoNumbersAddsCorrectlyCaseBigger(){
-        // Given
-        int test_a = 10210313;
-        boolean test_größer = true;
-
-        // When
-        boolean actual = Main.vierte(test_a);
-
-        // Then
-        assertEquals(test_größer, actual);
-    }
-
 }
