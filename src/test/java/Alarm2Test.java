397db0550ea762cd3a993bf530e3abb2f211ac26
Gregor Dirks <gregor_renke_adrian@mailbox.tu-dresden.de>
2022-05-22 21:15:54 +0200

    Alarm Tests hinzugefügt

--- src/test/java/Alarm2Test.java
+++ src/test/java/Alarm2Test.java
@@ -4,11 +4,95 @@ import org.junit.jupiter.api.Test;
 class Alarm2Test {
 
     @Test
-    void negativerWert() {
+    void leereEingabe() {
+        // Given
+        String test_Alarmstufe = "";
+        int test_a = 0;
+        String test_warnung = "bitte die Alarmstufe eintragen";
+
+        // When
+        String actual = Alarm2.warnung(test_Alarmstufe, test_a);
+
+        // Then
+        assertEquals(test_warnung, actual);
+    }
+
+    @Test
+    void rotImRahmen() {
         // Given
         String test_Alarmstufe = "rot";
-        int test_a = 23;
-        String test_warnung = "";
+        int test_a = 0;
+        String test_warnung = "Maximale Personenanzahl nicht überschritten";
+
+        // When
+        String actual = Alarm2.warnung(test_Alarmstufe, test_a);
+
+        // Then
+        assertEquals(test_warnung, actual);
+    }
+
+        @Test
+        void rotAusserRahmen() {
+            // Given
+            String test_Alarmstufe = "rot";
+            int test_a = 5;
+            String test_warnung = "Zu viele Personen";
+
+            // When
+            String actual = Alarm2.warnung(test_Alarmstufe, test_a);
+
+            // Then
+            assertEquals(test_warnung, actual);
+    }
+
+    @Test
+    void gelbImRahmen() {
+        // Given
+        String test_Alarmstufe = "gelb";
+        int test_a = 15;
+        String test_warnung = "Maximale Personenanzahl nicht überschritten";
+
+        // When
+        String actual = Alarm2.warnung(test_Alarmstufe, test_a);
+
+        // Then
+        assertEquals(test_warnung, actual);
+    }
+
+    @Test
+    void gelbAusserRahmen() {
+        // Given
+        String test_Alarmstufe = "gelb";
+        int test_a = 40;
+        String test_warnung = "Zu viele Personen";
+
+        // When
+        String actual = Alarm2.warnung(test_Alarmstufe, test_a);
+
+        // Then
+        assertEquals(test_warnung, actual);
+    }
+
+    @Test
+    void gruenImRahmen() {
+        // Given
+        String test_Alarmstufe = "grün";
+        int test_a = 40;
+        String test_warnung = "Maximale Personenanzahl nicht überschritten";
+
+        // When
+        String actual = Alarm2.warnung(test_Alarmstufe, test_a);
+
+        // Then
+        assertEquals(test_warnung, actual);
+    }
+
+    @Test
+    void gruenAusserRahmen() {
+        // Given
+        String test_Alarmstufe = "grün";
+        int test_a = 65;
+        String test_warnung = "Zu viele Personen";
 
         // When
         String actual = Alarm2.warnung(test_Alarmstufe, test_a);
